aadbd1595322a0ad920212fa800be747f5a5e95f
Imotytskiy <Imotytskiy@gmail.com>
2023-09-01 19:35:31 +0300

    add build

---
+++ InterviewPreparation/JS/script.js
@@ -0,0 +1,29 @@
+// SOLID
+
+// Single Responsibility Principle
+
+class Book {
+    constructor(title, author, content){
+        this.title = title;
+        this.author = author;
+        this.content = content;
+    }
+}
+
+class Person {
+    constructor(name, age) {
+      this.name = name;
+      this.age = age;
+    }
+  
+    sayHello() {
+      console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+    }
+  }
+  
+  // Instantiate an object from the class
+  const john = new Person('John', 30);
+  
+  // Call a method on the instance
+  john.sayHello();
+  console.log(john)
