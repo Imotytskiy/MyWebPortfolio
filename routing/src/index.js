e7ac2198f9a400a0e04e206b7795193c8caf070e
Imotytskiy <Imotytskiy@gmail.com>
2023-09-01 22:32:18 +0300

    add update

--- routing/src/index.js
+++ routing/src/index.js
@@ -29,7 +29,7 @@ const router = createBrowserRouter([
       loader: getPhotos,
     },
 ], 
-{ basename : '/Routing/' }  
+{ basename : '/Routing' }  
 );
 
 ReactDOM.createRoot(document.getElementById("root")).render(
